d828a029deb9c8690556a522e78d8fd761122035
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-16 13:37:14 -0300

    Change the way tokens/config are separated

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  colors: {
+    embed: 0x266B54,
+    error: 0xF03A17
+  }
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const tokens = require('./tokens.js');
 const config = require('./config.js');
 const utils = require('./utils.js');
 
 var betaMode = false;
-if (config.tokens.discord_beta) betaMode = true;
+if (tokens.discord_beta) betaMode = true;
 
 client.on('ready', () => {
   client.user.setGame('d!help');
@@ -26,10 +27,10 @@ client.on('message', message => {
 client.on('guildCreate', guild => {
   var embed = new Discord.RichEmbed;
   embed.addField(`Hi, i'm Deku!`, `I was made by **pedrofracassi#4623** to help organize your beautiful Guild.\n\nIf you want to know more about me, type \`d!help\`.\n\n<:izuku:358407294100439040> [Add me to your server](https://discordapp.com/oauth2/authorize?client_id=358398001233920001&scope=bot) | [Join my server](https://discord.gg/9W7yyBA)`);
-  embed.setColor(config.embedColor);
+  embed.setColor(config.colors.embed);
   embed.setThumbnail('https://i.imgur.com/lUVxkAK.png');
   guild.channels.filter(c=>c.type=='text').first().send({ embed });
 })
 
-var token = config.tokens.discord_beta || config.tokens.discord;
+var token = tokens.discord_beta || tokens.discord;
 client.login(token);
