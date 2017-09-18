776d8474133ba7eda9085e7eadf3122efad55604
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-18 19:47:19 -0300

    New useful function: arrayToStringWithCommas

--- utils.js
+++ utils.js
@@ -23,5 +23,18 @@ module.exports = {
     embed.setColor(config.colors.embed);
     embed.setFooter(message.author.tag, message.author.avatarURL);
     return embed;
+  },
+  arrayToStringWithCommas: function(array, last) {
+    var string = "";
+    for (i = 0; i < array.length; i++) {
+      if (i == (array.length - 1)) {
+        string = string + array[i];
+      } else if (i == (array.length - 2)) {
+        string = string + array[i] + last;
+      } else {
+        string = string + array[i] + ", ";
+      }
+    }
+    return string;
   }
 }
