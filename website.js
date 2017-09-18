7f73a17a71a5c1a55690bb107819d29a4d8fd993
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-18 15:46:02 -0300

    Fix website.js

--- website.js
+++ website.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const staticPath = path.join(__dirname, '/public');
+const staticPath = path.join(__dirname, '/docs');
 const port = process.env.PORT || 8080;
 
 const server = express().use(express.static(staticPath));
