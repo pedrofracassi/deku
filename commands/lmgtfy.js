b7db4bf748789f764bb2ad53d9b2421caab15d83
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-30 12:31:21 -0300

    Make d!lmgtfy translatable

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -1,4 +1,5 @@
 const utils = require('../utils.js');
+const cmdName = "lmgtfy";
 
 module.exports = {
   run: function(message, lang) {
@@ -6,10 +7,15 @@ module.exports = {
     var args = message.cleanContent.match(expression)[2].split(' ');
     var embed = utils.generateDekuDiv(message);
     var text = args.join(" ");
-    if(text.includes('--explain')) {
-      embed.setDescription('http://lmgtfy.com/?iie=1&q=' + encodeURI(text.replace('--explain', '')));
+    if (args[0]) {
+      if(text.includes('--explain')) {
+        embed.setDescription('http://lmgtfy.com/?iie=1&q=' + encodeURI(text.replace('--explain', '')));
+      } else {
+        embed.setDescription('http://lmgtfy.com/?q=' + encodeURI(text));
+      }
     } else {
-      embed.setDescription('http://lmgtfy.com/?q=' + encodeURI(text));
+      embed.setTitle(lang.commands[cmdName].no_question);
+      embed.setDescription(`\u200b\n${lang.usage} \`${lang.commands[cmdName]. _usage}\`\n${lang.example} \`${lang.commands[cmdName]._example}\``);
     }
     message.channel.send({embed});
   }
