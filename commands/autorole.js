f38dd66292d8857a827594d250b62bbfa548f8b2
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-10-02 19:42:24 -0300

    Require important stuff

--- commands/autorole.js
+++ commands/autorole.js
@@ -1,3 +1,7 @@
+const levelup = require('levelup');
+const utils = require('../utils.js');
+const config = require('../config.js');
+
 exports.run = function (message, lang, databases) {
   // body...
 };
