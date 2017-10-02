297a4dc25da3ad60db04348380e34b508e8cd0ad
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-10-02 20:36:01 -0300

    Core functionality

--- commands/autorole.js
+++ commands/autorole.js
@@ -2,6 +2,36 @@ const levelup = require('levelup');
 const utils = require('../utils.js');
 const config = require('../config.js');
 
+// d!autorole <role> [--bots]
+
 exports.run = function (message, lang, databases) {
-  // body...
+  var db = databases.autorole_config;
+  var embed = utils.generateDekuDiv(message);
+  var args = message.content.match(utils.expression)[2].split(' ');
+  if (args[0]) {
+    if (message.guild.roles.get(args[0])) {
+      if (message.member.hasPermission('MANAGE_GUILD')) {
+        var type = 'everyone';
+        if (args[1] == '--bots') type = 'bots';
+        db.get(message.guild.id, function (err, value) {
+          if (!value) value = "{}";
+          value = JSON.parse(value);
+          value[type] = args[0];
+          console.log(value);
+          db.put(message.guild.id, JSON.stringify(value), function (err) {
+            if (!err) {
+              message.reply('done, bro. ' + JSON.stringify(value));
+            } else {
+              message.reply('nope. ' + err);
+            }
+          });
+        });
+      }
+    } else {
+      embed.setColor(config.colors.error);
+      embed.setDescription('')
+    }
+  } else {
+    message.reply('role doesn\'t exist.');
+  }
 };
