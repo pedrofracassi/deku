5e215b2d3446ab18830c7c96a08aeca4f6da23e2
davipatury <davi.patury@yahoo.com.br>
2017-10-13 12:05:17 -0300

    Improves avatar command.

--- commands/avatar.js
+++ commands/avatar.js
@@ -9,10 +9,37 @@ module.exports = class Avatar extends Command {
   }
 
   run(message, args, commandLang) {
-    let member = message.mentions.members.first() || message.member;
-    let user   = member ? member.user : message.author;
-    let msg = user == message.author ? commandLang.own_picture : commandLang.someones_picture.replace('{0}', member ? member.displayName : user.name);
-    message.reply(`${msg}\n${user.avatarURL}`);
+    let embed = this.client.getDekuEmbed(message);
+    let query = args.join(" ").toLowerCase();
+    let mbr = this.findMember(query, message);
+    let msg = mbr.user == message.author ?
+              commandLang.own_picture :
+              commandLang.someones_picture.replace('{0}', mbr ? mbr.displayName : mbr.user.name);
+
+    embed.setDescription(`${message.author}, ${msg}\n${mbr.user.displayAvatarURL}`);
+    embed.setImage(mbr.user.displayAvatarURL);
+    message.channel.send({embed});
+  }
+
+  canRun(message) {
+    return message.guild ? true : false;
+  }
+
+  findMember(query, message) {
+    let member = null;
+    if(query) {
+      member = message.guild.members.find(m => {
+        return m.displayName.toLowerCase().startsWith(query) ||
+        m.user.username.toLowerCase().startsWith(query) ||
+        m.id == query
+      });
+    }
+
+    if(!member) {
+      member = message.mentions.members.first() || message.member;
+    }
+
+    return member;
   }
 
 }
