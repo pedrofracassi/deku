12b93850d476a7cae3f0f1974d583700d2b36a85
davipatury <davi.patury@yahoo.com.br>
2017-10-22 00:15:04 -0200

    More fixes...

--- structures/command.js
+++ structures/command.js
@@ -5,7 +5,15 @@ module.exports = class Command {
     this.aliases     = [];
     this.subcommands = [];
   }
-
+  
+  /**
+   * Check for subcommands, if finds execute it else executes the command itself
+   * @param {Message} message Message that triggered this command
+   * @param {Array<string>} args Command arguments
+   * @param {Object} commandLang Language object with strings for this command
+   * @param {Object} databases Bot's databases object
+   * @param {Object} lang Current guild's language object
+   */
   _run(message, args, commandLang, databases, lang) {
     if(args.length > 0) {
       let subcommand = this.subcommands.find(c => c.name.toLowerCase() == args[0] || c.aliases.includes(args[0]));
@@ -15,10 +23,24 @@ module.exports = class Command {
     }
     return this.run(message, args, commandLang, databases, lang)
   }
-
-  run(message, args, commandLang, databases, lang) {}
   
-  canRun(message, args) {
+  /**
+   * Execute this command
+   * @param {Message} message Message that triggered this command
+   * @param {Array<string>} args Command arguments
+   * @param {Object} commandLang Language object with strings for this command
+   * @param {Object} databases Bot's databases object
+   * @param {Object} lang Current guild's language object
+   */
+  run(message) {}
+  
+  /**
+   * Returns true if this command can be ran
+   * @param {Message} message Message that triggered this command
+   * @param {Array<string>} args Command arguments
+   * @returns {boolean} If this command can be ran
+   */
+  canRun() {
     return true
   }
   
