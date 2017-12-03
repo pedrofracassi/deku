e80b85d5140b618400f9b2b37d77c5d8aeb30974
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-12-03 14:07:48 -0300

    Add d!generateinvite

---
+++ commands/generateinvite.js
@@ -0,0 +1,36 @@
+const Command = require('../structures/command.js');
+
+module.exports = class BugReport extends Command {
+
+  constructor(client) {
+    super(client);
+    this.name    = "generateinvite";
+    this.aliases = ["geninvite", "geninv", "gi", "invitegen"];
+  }
+
+  run(message, args, commandLang, databases, lang) {
+    let embed = this.client.getDekuEmbed(message);
+    let options = {
+      unique: true
+    }
+    if (message.member.hasPermission('CREATE_INSTANT_INVITE')) {
+      message.channel.startTyping();
+      let channel = message.member.voiceChannel ? message.member.voiceChannel : message.channel;
+      channel.createInvite({options: options}, lang.reason).then(invite => {
+          embed.setTitle(commandLang.heres_your_invite.replace('{0}', channel.name)).setDescription('http://discord.gg/' + invite.code);
+          message.channel.send(embed);
+          message.channel.stopTyping();
+      });
+    } else {
+      embed.setTitle(lang.missing_invite_permission);
+      embed.setColor(this.client.config.colors.error);
+      message.channel.send(embed);
+    }
+
+  }
+
+}
+
+// Config flags
+// disable-command:generateinvite
+// generateinvite-cmd:allow-anyone
