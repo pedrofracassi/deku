94a8da1408619524a377947d27b7dadcd9428a8c
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-01-31 22:35:21 -0300

    Change what was requested by @davipatury

--- commands/generateinvite.js
+++ commands/generateinvite.js
@@ -17,7 +17,7 @@ module.exports = class BugReport extends Command {
       message.channel.startTyping();
       let channel = message.member.voiceChannel ? message.member.voiceChannel : message.channel;
       channel.createInvite({options: options}, lang.reason).then(invite => {
-          embed.setTitle(commandLang.heres_your_invite.replace('{0}', channel.name)).setDescription('http://discord.gg/' + invite.code);
+          embed.setTitle(commandLang.heres_your_invite.replace('{0}', channel.name)).setDescription(invite.url);
           message.channel.send(embed);
           message.channel.stopTyping();
       });
